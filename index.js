5627650448468128baef60e5d8f95aa4eb59230e
natalia.yelistratova <natalia.yelistratova@gmail.com>
2023-09-03 12:52:06 +0300

    Updated comments

--- index.js
+++ index.js
@@ -1,106 +1,105 @@
-// "use strict";
+"use strict";
 
-// const obj = {
-//     prop: 'Test',
-//     objMethod() {
-//         console.log(this.prop);
-//     }
-// }
+const obj = {
+    prop: 'Test',
+    objMethod() {
+        console.log(this.prop);
+    }
+}
 
-// obj.objMethod(); // Test
+// obj.objMethod();
 
 // Example 1
-// function sayHi() {
-//   console.log( this.name );
-// }
+function sayHi() {
+  console.log( this.name );
+}
 
-// let user = { name: "John" };
-// let admin = { name: "Admin" };
+let user = { name: "John" };
+let admin = { name: "Admin" };
 
-// // user['f'] = sayHi;
-// user.f = sayHi;
-// admin.f = sayHi;
+// user['f'] = sayHi;
+user.f = sayHi;
+admin.f = sayHi;
 
-// user.f(); // "John"
-// admin.f(); // "Admin"
+// user.f(); 
+// admin.f();
 
 // Example 2
-// class Person {
-//     constructor(name) {
-//         this.name = name
-//     }
-//     showName() {
-//         console.log(this.name)
-//     }
-// }
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+    showName() {
+        console.log(this.name)
+    }
+}
 
-// const member = new Person("John")
-// const member2 = new Person("Ivan")
-// member.showName() // "John"
-// member2.showName() // "Ivan"
+const member = new Person("John")
+const member2 = new Person("Ivan")
+// member.showName()
+// member2.showName()
 
 // Example 3
-// const name = 'Vasya';
+const name = 'Vasya';
 
-// function test() {
-//     console.log(this.name);
-// }
+function test() {
+    console.log(this.name);
+}
 
 // test(); 
 
 // Example 4
-// let user = {
-//     firstName: "Ivan",
-//     sayHi() {
-//         let arrow = () => {
-//             console.log(this.firstName);
-//         };
-//         arrow();
-//     },
-// };
+let user = {
+    firstName: "Ivan",
+    sayHi() {
+        let arrow = () => {
+            console.log(this.firstName);
+        };
+        arrow();
+    },
+};
 
-// user.sayHi(); // "Ivan"
+// user.sayHi(); 
 
-// let user1 = {
-//     firstName: "Ivan",
-//     sayHi() {
-//         function arrow() {
-//             console.log(this);
-//             console.log(this.firstName);
-//         };
-//         arrow();
-//     },
-// };
+let user1 = {
+    firstName: "Ivan",
+    sayHi() {
+        function arrow() {
+            console.log(this);
+            console.log(this.firstName);
+        };
+        arrow();
+    },
+};
 
-// user1.sayHi(); // window 
-//                // undefined 
+// user1.sayHi();
 
 // Example 5
-// const object = {
-//     message: 'Hello, World!',
-//     getMessage() {
-//       const message = 'Test';
-//       return message;
-//     }
-//   };
+const object = {
+    message: 'Hello, World!',
+    getMessage() {
+      const message = 'Test';
+      return message;
+    }
+  };
 
-// console.log(object.getMessage()); // 'Hello, World!'
+// console.log(object.getMessage());
 
 // Example 6
-// const fullname = 'John Doe';
-// const obj = {
-//    fullname: 'Colin Ihrig',
-//    prop: {
-//       fullname: 'Aurelio De Rosa',
-//       getFullname() {
-//          return this.fullname;
-//       }
-//    }
-// };
+const fullname = 'John Doe';
+const obj1 = {
+   fullname: 'Colin Ihrig',
+   prop: {
+      fullname: 'Aurelio De Rosa',
+      getFullname() {
+         return this.fullname;
+      }
+   }
+};
 
-// console.log(obj.prop.getFullname()); // 'Aurelio De Rosa'
+// console.log(obj1.prop.getFullname());
 
-// const test = obj.prop.getFullname;
+const test = obj1.prop.getFullname;
 
-// console.log(test()); // undefined
+// console.log(test());
 
